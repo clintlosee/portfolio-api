ed7cd514ebdaf1311c7e4bbb0cc670ab4058d186
Clint Losee <clint.losee@gmail.com>
2021-03-04 11:49:05 -0700

    Initial app setup

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '474ca4853a897709e81418e3956b03e0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ef27772f-d79b-4d8c-b6bb-13ae35a9603c'
+};

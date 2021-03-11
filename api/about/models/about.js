c5265b85a9a59111ad9a288a870b80c3cf934814
Clint Losee <clint.losee@gmail.com>
2021-03-11 11:09:33 -0700

    Initial setup for github actions

--- api/about/models/about.js
+++ api/about/models/about.js
@@ -1,8 +1,29 @@
 'use strict';
+const axios = require('axios');
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    async afterCreate(data) {
+      const url = 'https://api.github.com/repos/clintlosee/portfolio/dispatches';
+
+      const token = process.env.GITHUB_TOKEN;
+
+      const headers = {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+        Accept: 'application/vnd.github.v3+json'
+      };
+
+      const axiosResult = await axios.post(
+        url,
+        { event_type: 'created' },
+        { headers }
+      );
+    },
+  },
+};
